42e4bcf9cfbc648b86ece655a958e615eae85656
nikhil_yadav <166252753+Nikhil-Yadav97@users.noreply.github.com>
2026-08-20 00:07:52 +0530

    Time: 0 ms (100%), Space: 11.2 MB (19.66%) - LeetHub

--- 0397-integer-replacement/0397-integer-replacement.cpp
+++ 0397-integer-replacement/0397-integer-replacement.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    map<int,int> dp;
+    map<int, int> dp;
     int fn(long long n) {
         if (n == 1)
             return 0;
 
-        if (dp.find(n)!=dp.end())
+        if (dp.find(n) != dp.end())
             return dp[n];
 
         if (n % 2 == 0)
@@ -13,7 +13,7 @@ public:
         return dp[n] = 1 + min(fn(n - 1), fn(n + 1));
     }
     int integerReplacement(int n) {
-       dp.clear();
+        dp.clear();
         return fn(n);
     }
 };
